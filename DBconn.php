e49b1b6b5e40242099bd1c12a6bcd4746f46f81c
ronewa <ronewamaselesele200@gmail.com>
2024-10-13 00:25:58 +0200

    moving the Database to azure devops platform , but there errors

--- DBconn.php
+++ DBconn.php
@@ -1,5 +1,5 @@
 <?php
-    $servername = "localhost";
+    /*$servername = "localhost";
     $username = "root";
     $password = "";
     $dbname = "computer_complex";
@@ -9,3 +9,18 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
+        */
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:disappdd.database.windows.net,1433; Database = ComputerCmplex", "st10107568", "dianaK1209$");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "st10107568", "pwd" => "dianaK1209$", "Database" => "ComputerCmplex", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:disappdd.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
